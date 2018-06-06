d383e44c939186a56ac51cf0899e73dadaac47d9
David Markel <davidgmarkel@gmail.com>
2018-06-06 18:53:05 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
 
   def self.current_user(session)
-    User.find_by(session[:id])
+    User.find_by(session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    !!User.find_by(session[:id])
+    !!User.find_by(session[:user_id])
   end
 
 end
