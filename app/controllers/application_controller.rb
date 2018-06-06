2a6fd61061205b09f951058e0c952eb350ef1fa9
David Markel <davidgmarkel@gmail.com>
2018-06-06 19:13:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    binding.pry
     if Helpers.is_logged_in?(session)
       @user = User.find(session[:user_id])
       erb :account
